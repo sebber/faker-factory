4563b93223c9b836e31f1fa5c5c20df53cf97fa7
Sebastian Bergman <basse@grafikfel.org>
2023-03-24 11:17:32 +0100

    use a test context to de-duplicate blueprint

--- src/Factory.test.ts
+++ src/Factory.test.ts
@@ -1,49 +1,45 @@
-import { describe, it, expect } from "vitest";
-import { Factory } from "./Factory";
+import { describe, it, expect, beforeAll, beforeEach } from "vitest";
+import { Factory, FactoryDefinition } from "./Factory";
 
 type Car = {
   maker: string;
   modelYear: number;
 };
 
-describe("Factory", () => {
-  describe("given a blueprint", () => {
-    it("should be able to create one item of", () => {
-      const carFactory = Factory<Car>((faker) => ({
-        maker: faker.company.name(),
-        modelYear: faker.date.between("1769-01-01", new Date()).getFullYear(),
-      }));
+type TestContext = { carFactory: FactoryDefinition<Car> };
 
-      const car = carFactory.make();
+describe("Factory, given a blueprint of Car", () => {
+  beforeEach<TestContext>((ctx) => {
+    ctx.carFactory = Factory<Car>((faker) => ({
+      maker: faker.company.name(),
+      modelYear: faker.date.between("1769-01-01", new Date()).getFullYear(),
+    }));
+  });
 
+  it<TestContext>("should be able to create one Car", ({ carFactory }) => {
+    const car = carFactory.make();
+
+    expect(typeof car.maker).toBe(typeof "string");
+    expect(typeof car.modelYear).toBe(typeof 1);
+  });
+
+  it<TestContext>("should be able to create multiple Cars", ({
+    carFactory,
+  }) => {
+    const cars = carFactory.makeMany(5);
+
+    cars.forEach((car) => {
       expect(typeof car.maker).toBe(typeof "string");
       expect(typeof car.modelYear).toBe(typeof 1);
     });
+  });
 
-    it("should be able to create multiple items of", () => {
-      const carFactory = Factory<Car>((faker) => ({
-        maker: faker.company.name(),
-        modelYear: faker.date.between("1769-01-01", new Date()).getFullYear(),
-      }));
+  it<TestContext>("should be extendable for more detailed specifics", ({
+    carFactory,
+  }) => {
+    const volvoFactory = carFactory.with({ maker: "Volvo Cars" });
+    const volvo = volvoFactory.make();
 
-      const cars = carFactory.makeMany(5);
-
-      cars.forEach((car) => {
-        expect(typeof car.maker).toBe(typeof "string");
-        expect(typeof car.modelYear).toBe(typeof 1);
-      });
-    });
-
-    it("should be extendable", () => {
-      const carFactory = Factory<Car>((faker) => ({
-        maker: faker.company.name(),
-        modelYear: faker.date.between("1769-01-01", new Date()).getFullYear(),
-      }));
-
-      const volvoFactory = carFactory.with({ maker: "Volvo Cars" });
-      const volvo = volvoFactory.make();
-
-      expect(volvo.maker).toBe("Volvo Cars");
-    });
+    expect(volvo.maker).toBe("Volvo Cars");
   });
 });
